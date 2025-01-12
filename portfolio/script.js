5c4ae377a974e70d4df887aec178b51df54596c3
reshmakuna0 <reshmakuna03@gmail.com>
2025-01-12 20:38:52 +0530

    updated

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+let see = document.getElementById("btn");
+document.getElementById("foot").classList.add("hide");
+
+function opendetails(){
+    document.getElementById("foot").classList.remove("hide");
+    see.classList.add("hide");
+}
